ded3f68be7388d5fe0cb06c3a871b5a6bcb4efbf
O O <nkwadioo@gmail.com>
2022-08-03 07:51:12 +0200

    testBed for service

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 
 
 import * as express from 'express';
+import {Request, Response} from 'express';
 import {Application} from "express";
 import {getAllCourses, getCourseById} from "./get-courses.route";
 import {searchLessons} from "./search-lessons.route";
@@ -16,6 +17,7 @@ app.use(cors({origin: true}));
 
 app.use(bodyParser.json());
 
+const port = 9000;
 
 app.route('/api/courses').get(getAllCourses);
 
@@ -25,10 +27,24 @@ app.route('/api/lessons').get(searchLessons);
 
 app.route('/api/courses/:id').put(saveCourse);
 
-
-const httpServer:any = app.listen(9000, () => {
+app.route('/').get(Home);
+const httpServer:any = app.listen(port, () => {
     console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
 });
+
+function Home (req: Request, res: Response) {
+    res.status(200).send(`
+        <h1 style="align-text: center;text-align: center;width: 19ch;margin: auto;">Welcome to</br>Angular Typescript Testing Masterclass API 🚀</h1>
+
+        <p>Available links</p>
+        <ul>
+            <li><a href="http://localhost:${port}/api/courses" > Get "/api/courses" </a></li>
+            <li><a href="http://localhost:${port}/api/courses/1" > Get "/api/courses/:id" </a></li>
+            <li><a href="http://localhost:${port}/api/lessons" > Get "/api/lessons" </a></li>
+            <li><a href="http://localhost:${port}/api/courses/1" > Put "/api/courses/:id" </a></li>
+        </ul>
+    `)
+}
 
 
 
